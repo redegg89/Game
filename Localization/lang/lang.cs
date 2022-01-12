e525ed7385355671cc3895252222600f8ac2957f
redegg89 <78121676+redegg89@users.noreply.github.com>
2022-01-11 21:37:19 -0500

    Add Localization source files

---
+++ Localization/lang/lang.cs
@@ -0,0 +1,14 @@
+﻿namespace locale
+{
+    public class lang
+    {
+        public static string Speed;
+        public static string GlobalTimer;
+
+        public static void initDebug()
+        {
+            Speed = "Speed";
+            GlobalTimer = "Global Timer";
+        }
+    }
+}
